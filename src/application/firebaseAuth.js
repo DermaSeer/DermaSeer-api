b8a2f3e4c74b045613e362910b4e8a8aed6c397c
Rojak Kurniawan <rojakkurniawan4@gmail.com>
2024-11-20 02:53:45 +0700

    chore: delete value

--- src/application/firebaseAuth.js
+++ src/application/firebaseAuth.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzGLfvmS424qdmFWk-5dyT1V493xY_22Y",
-  authDomain: "dermaseer-auth.firebaseapp.com",
-  projectId: "dermaseer-auth",
-  storageBucket: "dermaseer-auth.appspot.com",
-  messagingSenderId: "773749053838",
-  appId: "1:773749053838:web:cfcd8395815d7fecda2cdf",
-  measurementId: "G-QJWPSV81ZW",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
