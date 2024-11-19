ad6d786f5a7d24692f90d646f7ac8d7a80a56263
Rojak Kurniawan <rojakkurniawan4@gmail.com>
2024-11-20 02:51:08 +0700

    feat: separate credentials service account

--- src/application/cloudStorage.js
+++ src/application/cloudStorage.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const serviceAccountPath = path.resolve(__dirname, "../application/serviceAccountCloudStorage.json");
+const serviceAccountPath = path.resolve(__dirname, "../application/credentials/cloudStorage.json");
 
 // Inisialisasi Google Cloud Storage
 const storage = new Storage({
--- src/application/firebaseAdmin.js
+++ src/application/firebaseAdmin.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const serviceAccountPath = path.resolve(__dirname, "../application/serviceAccountKey.json");
+const serviceAccountPath = path.resolve(__dirname, "../application/credentials/firebaseAdmin.json");
 const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 
 admin.initializeApp({
