f1ee9157a1486f7a9a0aadc586f3b550086447c5
mr-fadh <mr.faadhillah@gmail.com>
2024-11-21 00:22:54 +0700

    fix: publish_date contain null value

--- prisma/migrations/20241120170516_add_publish_date/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `publish_date` to the `articles` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "articles" ADD COLUMN     "publish_date" DATE NOT NULL;
---
+++ prisma/migrations/20241120172109_add_publish_date/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "articles" ADD COLUMN     "publish_date" DATE;
